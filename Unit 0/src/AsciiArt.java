243b9c4584ec490b6734bbc3fe5d8000852913d8
Matrixmaverick <78127538+Matrixmaverick@users.noreply.github.com>
2021-01-30 00:50:25 -0800

    Unit00 Files

---
+++ Unit 0/src/AsciiArt.java
@@ -0,0 +1,25 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - James Wright
+//Date - 1/29/21
+//Class - AP Comp Sci A
+//Lab  - Ascii Art
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("James W \n\n" );
+		System.out.println("I drew a dog." );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("               ___      _           " );
+		System.out.println("              °'')}____//           " );
+		System.out.println("              \'|_/ \\    \")      " );
+		System.out.println("                (_(_/-(_/       ");
+		System.out.println("                             ");
+		//add other output
+ 
+	}
+}
---
+++ Unit 0/src/AsciiBox.java
@@ -0,0 +1,45 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - James Wright	
+//Date - 1/29/21
+//Class - AP Comp Sci A
+//Lab  - Ascii Box
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		char let=65;
+		char lets=85;
+		System.out.println("James W \t  1/29/21 \n\n" );
+		System.out.println("++++++++++" );
+		System.out.println("++++++++++" );
+		System.out.println("++++++++++" );
+		System.out.println("++++++++++" );
+		System.out.print(let);
+		System.out.print(let);
+		System.out.print(let);
+		System.out.print(let);
+		System.out.print(let);
+		System.out.print(let);
+		System.out.print(let);
+		System.out.print(let);
+		System.out.print(let);
+		System.out.println(let);
+		System.out.println("++++++++++" );
+		System.out.println("++++++++++" );
+		System.out.println("++++++++++" );
+		System.out.println("++++++++++" );
+		System.out.print(lets);
+		System.out.print(lets);
+		System.out.print(lets);
+		System.out.print(lets);
+		System.out.print(lets);
+		System.out.print(lets);
+		System.out.print(lets);
+		System.out.print(lets);
+		System.out.print(lets);
+		System.out.print(lets);
+	}
+}
